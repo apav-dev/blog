16da5387197a466edd23bb9b73555e53ac1ced58
Aaron Pavlick <apavlick@yext.com>
2023-07-26 09:08:35 -0400

    commenting out code

--- src/templates/blog.tsx
+++ src/templates/blog.tsx
@@ -13,32 +13,33 @@ import BlogLayout from "../components/BlogLayout";
 import Main from "../components/layouts/Main";
 import { formatDate } from "../utils/formatDate";
 
-export const config: TemplateConfig = {
-  stream: {
-    $id: "blog",
-    fields: [
-      "id",
-      "name",
-      "slug",
-      "datePosted",
-      "c_coverPhoto",
-      "c_body",
-      "c_description",
-      "c_metaDescription",
-      "c_keywords",
-    ],
-    filter: {
-      entityTypes: ["ce_blog"],
-    },
-    localization: {
-      locales: ["en"],
-      primary: false,
-    },
-  },
-};
+// export const config: TemplateConfig = {
+//   stream: {
+//     $id: "blog",
+//     fields: [
+//       "id",
+//       "name",
+//       "slug",
+//       "datePosted",
+//       "c_coverPhoto",
+//       "c_body",
+//       "c_description",
+//       "c_metaDescription",
+//       "c_keywords",
+//     ],
+//     filter: {
+//       entityTypes: ["ce_blog"],
+//     },
+//     localization: {
+//       locales: ["en"],
+//       primary: false,
+//     },
+//   },
+// };
 
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
-  return document.slug;
+  // return document.slug;
+  return "blog";
 };
 
 export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
@@ -48,39 +49,40 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
     title: document.name,
     charset: "UTF-8",
     viewport: "width=device-width, initial-scale=1",
-    tags: [
-      {
-        type: "meta",
-        attributes: {
-          name: "description",
-          content: document.c_metaDescription,
-        },
-      },
-      {
-        type: "meta",
-        attributes: {
-          name: "keywords",
-          content: document.c_keywords,
-        },
-      },
-    ],
+    // tags: [
+    //   {
+    //     type: "meta",
+    //     attributes: {
+    //       name: "description",
+    //       content: document.c_metaDescription,
+    //     },
+    //   },
+    //   {
+    //     type: "meta",
+    //     attributes: {
+    //       name: "keywords",
+    //       content: document.c_keywords,
+    //     },
+    //   },
+    // ],
   };
 };
 
 const Blog = ({ document }: TemplateRenderProps) => {
-  const { name, datePosted, c_body, c_coverPhoto } = document;
+  // const { name, datePosted, c_body, c_coverPhoto } = document;
 
   return (
-    <Main>
-      <Container>
-        <BlogLayout
-          title={name}
-          date={formatDate(datePosted)}
-          content={c_body}
-          image={c_coverPhoto}
-        />
-      </Container>
-    </Main>
+    <h1>Hello from Blog!</h1>
+    // <Main>
+    //   <Container>
+    //     <BlogLayout
+    //       title={name}
+    //       date={formatDate(datePosted)}
+    //       content={c_body}
+    //       image={c_coverPhoto}
+    //     />
+    //   </Container>
+    // </Main>
   );
 };
 
